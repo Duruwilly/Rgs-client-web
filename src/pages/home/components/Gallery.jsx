475d664bfc4a1f2552b570009dfe2ab8e45b478e
princewill-broadshift <princewill.d@broadshift.com>
2023-05-04 23:53:15 +0100

    gallery ft: popup image

--- src/pages/home/components/Gallery.jsx
+++ src/pages/home/components/Gallery.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import room2 from "../../../assets/images/room2.jpg";
-import room1 from "../../../assets/images/room.jpg";
+import room3 from "../../../assets/images/room6.jpg";
+import { VscClose } from "react-icons/vsc";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
 const Gallery = () => {
   const options = {
@@ -28,8 +30,42 @@ const Gallery = () => {
       1100: {
         items: 3,
       },
-      
     },
+  };
+
+  const galleryPhotos = [
+    {
+      id: 1,
+      src: room2,
+    },
+    {
+      id: 2,
+      src: room2,
+    },
+    {
+      id: 3,
+      src: room3,
+    },
+  ];
+
+  const [sliderNumber, setSliderNumber] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = (index) => {
+    setSliderNumber(index);
+    setOpen(true);
+  };
+
+  const handleMove = (dir) => {
+    let newSliderNumber;
+
+    if (dir === "l") {
+      newSliderNumber = sliderNumber === 0 ? 2 : sliderNumber - 1;
+    } else {
+      newSliderNumber = sliderNumber === 2 ? 0 : sliderNumber + 1;
+    }
+
+    setSliderNumber(newSliderNumber);
   };
 
   return (
@@ -38,13 +74,81 @@ const Gallery = () => {
         our gallery
         <span className="line"></span>
       </h3>
+      {open && (
+        // <div className="slider">
+        //   <div
+        //     className="h-10 w-10 flex items-center justify-center rounded-full bg-red-900 text-white absolute top-5 right-5 cursor-pointer"
+        //     onClick={() => setOpen(false)}
+        //   >
+        //     <AiOutlineClose />
+        //   </div>
+        //   <div
+        //     className="h-10 w-10 flex items-center justify-center rounded-full bg-red-900 text-white absolute left-5 cursor-pointer"
+        //     onClick={() => handleMove("l")}
+        //   >
+        //     <AiOutlineArrowLeft className="" />
+        //   </div>
+        //   <div
+        //     className="h-10 w-10 flex items-center justify-center rounded-full bg-red-900 text-white absolute right-5 cursor-pointer"
+        //     onClick={() => handleMove("r")}
+        //   >
+        //     <AiOutlineArrowRight />
+        //   </div>
+        //   <div className="sliderWrapper">
+        //     <img
+        //       src={galleryPhotos[sliderNumber].src}
+        //       alt=""
+        //       className="sliderImg"
+        //     />
+        //   </div>
+        // </div>
+        <div
+          className=" w-screen h-screen fixed top-0 left-0 flex items-center justify-center z-[100] px-2"
+          style={{ background: "rgba(0, 0, 0, 0.8)" }}
+        >
+          <div className="w-full max-w-screen-lg relative shadow-md overflow-y-auto">
+            <div className="absolute top-0 right-0">
+              <button
+                className={`rounded-full w-12 h-12 p-0 border-0 inline-flex items-center justify-center text-4xl text-white opacity-70 hover:text-white hover:opacity-100`}
+                style={{ background: "rgba(0,0,0,0.4)" }}
+              >
+                <VscClose className="" onClick={() => setOpen(false)} />
+              </button>
+            </div>
+            <button className="text-4xl absolute left-0 top-[50%] cursor-pointer text-white opacity-70 hover:text-white hover:opacity-100 z-20">
+              <SlArrowLeft className="" onClick={() => handleMove("l")} />
+            </button>
+
+            <button className="text-4xl absolute  -right-0 top-[50%] cursor-pointer text-white opacity-70 hover:text-white hover:opacity-100 z-20">
+              <SlArrowRight onClick={() => handleMove("r")} />
+            </button>
+            <div className="sliderWrapper">
+              <img
+                src={galleryPhotos[sliderNumber].src}
+                alt=""
+                className="sliderImg"
+              />
+            </div>
+          </div>
+        </div>
+      )}
       <div className="w-full justify-center items-center px- mt-10">
         <OwlCarousel className="owl-carousel owl-theme" {...options}>
+          {/* <img src={room2} alt="" />
           <img src={room2} alt="" />
           <img src={room2} alt="" />
           <img src={room2} alt="" />
-          <img src={room2} alt="" />
-          <img src={room2} alt="" />
+          <img src={room2} alt="" /> */}
+          {galleryPhotos.map((photo, index) => (
+            <div key={index}>
+              <img
+                onClick={() => handleOpen(index)}
+                src={photo.src}
+                alt="gallery"
+                className="hover:scale-105 duration-500 cursor-pointer"
+              />
+            </div>
+          ))}
         </OwlCarousel>
       </div>
     </section>
